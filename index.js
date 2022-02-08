5e99b33c740233f6c492232f3a110eddbc13f24c
Gregor Parzefall <gregor.parzefall@posteo.de>
2022-02-08 18:00:27 +0100

    First commit

---
+++ index.js
@@ -0,0 +1,91 @@
+#!/usr/bin/env node
+
+import { readdir } from 'fs/promises';
+import { join } from 'path';
+
+import Database from 'better-sqlite3';
+import { lookup } from 'mime-types';
+import { exiftool, ExifDateTime } from 'exiftool-vendored';
+
+const db = new Database(process.argv[2]);
+
+const initStatement = db.prepare(
+    `CREATE TABLE 'photos' (
+		'id' INTEGER NOT NULL,
+
+		'date' TEXT,
+		'deviceMake' TEXT,
+		'deviceModel' TEXT,
+        'software' TEXT,
+
+		'fileType' TEXT NOT NULL,
+		'filePath' TEXT NOT NULL,
+
+		PRIMARY KEY ('id' AUTOINCREMENT)
+	);`,
+);
+initStatement.run();
+
+const insertStatement = db.prepare(
+    `INSERT INTO 'photos' (
+		'date',
+		'deviceMake',
+		'deviceModel',
+        'software',
+
+		'fileType',
+		'filePath'
+	) VALUES (
+		?,
+		?,
+		?,
+        ?,
+
+		?,
+		?
+	)`,
+);
+
+const processDir = async (path) => {
+    const entries = await readdir(path, {
+        withFileTypes: true,
+    });
+
+    await Promise.all(
+        entries.map(async (entry) => {
+            const entryPath = join(path, entry.name);
+
+            if (entry.isDirectory()) {
+                await processDir(entryPath);
+            }
+
+            if (entry.isFile()) {
+                await processFile(entryPath);
+            }
+        }),
+    );
+};
+
+const processFile = async (path) => {
+    const type = lookup(path) || 'application/octet-stream';
+
+    if (type.startsWith('image/')) {
+        const exif = await exiftool.read(path);
+
+        insertStatement.run(
+            exif.DateTimeOriginal instanceof ExifDateTime
+                ? exif.DateTimeOriginal.toISOString()
+                : null,
+            exif.Make,
+            exif.Model,
+            exif.Software,
+
+            type,
+            path,
+        );
+    }
+};
+
+await processDir(process.argv[3]);
+
+exiftool.end();
